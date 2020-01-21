783d4470e0f9779c5b38734447dd3dbbce7d80da
Asif Kamal <asif_kamal@hotmail.com>
2020-01-21 20:54:44 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Transfer
 
 attr_reader :status
@@ -19,8 +21,25 @@ def valid?
 end
 
 def execute_transaction
-  @sender.balance - amount
-  @receiver.balance + amount
-  
+  @sender.balance -= amount
+  @receiver.balance += amount
+  @amount = 0
+  @status = "complete"
+  if @receiver.valid? === false || @sender.valid? === false
+     @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end
+
+
+def reverse_transfer
+  if self.execute_transaction
+  @receiver.balance - @sender.balance = @amount
+  @sender.balance += @amount
+  @receiver.balance -= @amount
+  @status = "reversed"
+  binding.pry
+end
+end
+
 end
