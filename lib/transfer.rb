884dd197b72e84bd6e63a7c18c162edd3ba8a723
Asif Kamal <asif_kamal@hotmail.com>
2020-01-21 21:11:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,8 +34,8 @@ end
 
 def reverse_transfer
   if @status = "complete"
-  @sender.balance += @amount
-  @receiver.balance -= @amount
+  @sender.balance += amount
+  @receiver.balance -= amount
   @status = "reversed"
 end
 end
