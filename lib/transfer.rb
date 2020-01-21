8a54cea0b0628f65fc5dace99f463dd040d74552
Asif Kamal <asif_kamal@hotmail.com>
2020-01-21 19:51:13 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
 class Transfer
-  # your code here
+
+attr_reader :status
+attr_accessor :sender, :receiver, :amount
+
+def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount 
+  @status = "pending"
+end
+
+def valid?
+  if @sender.valid? && @receiver.valid?
+      true 
+  else
+    false 
+  end
+end
+
+def execute_transaction
+  @sender.balance - amount
+  @receiver.balance + amount
+  
+end
 end
